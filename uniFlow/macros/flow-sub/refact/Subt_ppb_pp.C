f8d7958462b8ca5d82a3ea4ccbe2318b6f0f62ff
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-01-27 11:29:52 +0100

    Subt_ppb_pp done for dn{2} lvl

--- uniFlow/macros/flow-sub/refact/Subt_ppb_pp.C
+++ uniFlow/macros/flow-sub/refact/Subt_ppb_pp.C
@@ -14,6 +14,7 @@ void StyleHist(TH1* hist, Color_t color = kRed, Style_t markerStyle = kOpenCircl
 TH1D* Scale(TH1D* base, Double_t factor);
 TH1D* Subtract(TH1D* raw, TH1D* base, Double_t factor = 1.0);
 
+// colors for centrality
 Color_t colors[] = {kGreen+2, kBlue, kBlack, kMagenta+1};
 
 
@@ -37,106 +38,107 @@ void Subt_ppb_pp()
   gSystem->mkdir(sOutFolder,kTRUE);
 
   // LOADING INPUT
-  // raw pPb cent dep
-  TFile* fileInRaw = OpenFile(sInFileRaw+"/"+sMethod+"/Processed.root"); if(!fileInRaw) { return; }
-  TH1D* hRaw_Cum_Charged_cent0 = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",0),fileInRaw); if(!hRaw_Cum_Charged_cent0) { return; }
-  TH1D* hRaw_Cum_Charged_cent1 = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",1),fileInRaw); if(!hRaw_Cum_Charged_cent1) { return; }
-  TH1D* hRaw_Cum_Charged_cent2 = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",2),fileInRaw); if(!hRaw_Cum_Charged_cent2) { return; }
-  TH1D* hRaw_Cum_Charged_cent3 = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",3),fileInRaw); if(!hRaw_Cum_Charged_cent3) { return; }
 
+  TFile* fileInRaw = OpenFile(sInFileRaw+"/"+sMethod+"/Processed.root"); if(!fileInRaw) { return; }
+  TFile* fileInBase = OpenFile(sInFileBase+"/"+sMethod+"/Processed.root"); if(!fileInBase) { return; }
+  TFile* fileInBaseInt = OpenFile(sInFileBaseInt+"/"+sMethod+"/Processed.root"); if(!fileInBaseInt) { return; }
+
+  TH1D* hBase_Cum_Charged_int = LoadHisto("hCum2_Charged_harm2_gap08_cent0",fileInBaseInt); if(!hBase_Cum_Charged_int) { return; }
+  StyleHist(hBase_Cum_Charged_int, kRed, kOpenCircle);
+
+  TList* list_Raw_Cum_Charged = new TList();
+  TList* list_Base_Cum_Charged = new TList();
+
+  for(Int_t cent(0); cent < iNumCent; ++cent)
+  {
+    TH1D* temp = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",cent),fileInRaw); if(!temp) { return; }
+    StyleHist(temp, colors[cent], kFullCircle);
+    list_Raw_Cum_Charged->Add(temp);
+
+    temp = LoadHisto(Form("hCum2_Charged_harm2_gap08_cent%d",cent),fileInBase); if(!temp) { return; }
+    StyleHist(temp, colors[cent], kOpenSquare);
+    list_Base_Cum_Charged->Add(temp);
+  }
+
+  // multiplicities
   TFile* fileInRaw_Mult = OpenFile(sInFileRaw+"/Mult.root"); if(!fileInRaw_Mult) { return; }
   TProfile* hRaw_Mult = (TProfile*) LoadHisto("fpRefsMult_rebin",fileInRaw_Mult); if(!hRaw_Mult) { return; }
 
-  // base pp cent int
-  TFile* fileInBaseInt = OpenFile(sInFileBaseInt+"/"+sMethod+"/Processed.root"); if(!fileInBaseInt) { return; }
-  TH1D* hBase_Cum_Charged_int = LoadHisto("hCum2_Charged_harm2_gap08_cent0",fileInBaseInt); if(!hBase_Cum_Charged_int) { return; }
+  TFile* fileInBase_Mult = OpenFile(sInFileBase+"/Mult.root"); if(!fileInBase_Mult) { return; }
+  TProfile* hBase_Mult = (TProfile*) LoadHisto("fpRefsMult_rebin",fileInBase_Mult); if(!hBase_Mult) { return; }
 
   TFile* fileInBase_MultInt = OpenFile(sInFileBaseInt+"/Mult.root"); if(!fileInBase_MultInt) { return; }
   TProfile* hBase_MultInt = (TProfile*) LoadHisto("fpRefsMult_rebin",fileInBase_MultInt); if(!hBase_MultInt) { return; }
   Double_t dMult_Base_Int = hBase_MultInt->GetBinContent(1);
 
-  // base pp cent dependetn
-  TFile* fileInBase = OpenFile(sInFileBase+"/"+sMethod+"/Processed.root"); if(!fileInBase) { return; }
-  TH1D* hBase_Cum_Charged_cent0 = LoadHisto("hCum2_Charged_harm2_gap08_cent0",fileInBase); if(!hBase_Cum_Charged_cent0) { return; }
-  TH1D* hBase_Cum_Charged_cent1 = LoadHisto("hCum2_Charged_harm2_gap08_cent1",fileInBase); if(!hBase_Cum_Charged_cent1) { return; }
-  TH1D* hBase_Cum_Charged_cent2 = LoadHisto("hCum2_Charged_harm2_gap08_cent2",fileInBase); if(!hBase_Cum_Charged_cent2) { return; }
-  TH1D* hBase_Cum_Charged_cent3 = LoadHisto("hCum2_Charged_harm2_gap08_cent3",fileInBase); if(!hBase_Cum_Charged_cent3) { return; }
-
-  TFile* fileInBase_Mult = OpenFile(sInFileBase+"/Mult.root"); if(!fileInBase_Mult) { return; }
-  TProfile* hBase_Mult = (TProfile*) LoadHisto("fpRefsMult_rebin",fileInBase_Mult); if(!hBase_Mult) { return; }
-
-
-  // settiing plotting setting
-  StyleHist(hRaw_Cum_Charged_cent0, kRed, kFullCircle);
-  StyleHist(hRaw_Cum_Charged_cent1, kRed, kFullCircle);
-  StyleHist(hRaw_Cum_Charged_cent2, kRed, kFullCircle);
-  StyleHist(hRaw_Cum_Charged_cent3, kRed, kFullCircle);
-
-  StyleHist(hBase_Cum_Charged_int, kRed, kOpenCircle);
-  StyleHist(hBase_Cum_Charged_cent0, colors[0], kOpenSquare);
-  StyleHist(hBase_Cum_Charged_cent1, colors[1], kOpenSquare);
-  StyleHist(hBase_Cum_Charged_cent2, colors[2], kOpenSquare);
-  StyleHist(hBase_Cum_Charged_cent3, colors[3], kOpenSquare);
-
   // scaling base with pre-factor
-  TH1D* hRaw_Cum_Charged_cent0_scaled = Scale(hRaw_Cum_Charged_cent0, hRaw_Mult->GetBinContent(0+1));
-  TH1D* hRaw_Cum_Charged_cent1_scaled = Scale(hRaw_Cum_Charged_cent1, hRaw_Mult->GetBinContent(1+1));
-  TH1D* hRaw_Cum_Charged_cent2_scaled = Scale(hRaw_Cum_Charged_cent2, hRaw_Mult->GetBinContent(2+1));
-  TH1D* hRaw_Cum_Charged_cent3_scaled = Scale(hRaw_Cum_Charged_cent3, hRaw_Mult->GetBinContent(3+1));
+  TH1D* hBase_Cum_Charged_int_scaled = Scale(hBase_Cum_Charged_int, hBase_MultInt->GetBinContent(1));
 
-  TH1D* hBase_Cum_Charged_int_scaled = Scale(hBase_Cum_Charged_int, hBase_MultInt->GetBinContent(0+1));
-  TH1D* hBase_Cum_Charged_cent0_scaled = Scale(hBase_Cum_Charged_cent0, hBase_Mult->GetBinContent(0+1));
-  TH1D* hBase_Cum_Charged_cent1_scaled = Scale(hBase_Cum_Charged_cent1, hBase_Mult->GetBinContent(1+1));
-  TH1D* hBase_Cum_Charged_cent2_scaled = Scale(hBase_Cum_Charged_cent2, hBase_Mult->GetBinContent(2+1));
-  TH1D* hBase_Cum_Charged_cent3_scaled = Scale(hBase_Cum_Charged_cent3, hBase_Mult->GetBinContent(3+1));
+  TList* list_Raw_Cum_Charged_scaled = new TList();
+  TList* list_Base_Cum_Charged_scaled = new TList();
 
-  TH1D* hSubPP_Cum_Charged_int = Subtract(hRaw_Cum_Charged_cent0_scaled, hBase_Cum_Charged_int_scaled);
-  TH1D* hSubPP_Cum_Charged_cent0 = Subtract(hRaw_Cum_Charged_cent0_scaled, hBase_Cum_Charged_cent0_scaled);
-  TH1D* hSubPP_Cum_Charged_cent1 = Subtract(hRaw_Cum_Charged_cent0_scaled, hBase_Cum_Charged_cent1_scaled);
-  TH1D* hSubPP_Cum_Charged_cent2 = Subtract(hRaw_Cum_Charged_cent0_scaled, hBase_Cum_Charged_cent2_scaled);
-  TH1D* hSubPP_Cum_Charged_cent3 = Subtract(hRaw_Cum_Charged_cent0_scaled, hBase_Cum_Charged_cent3_scaled);
+  for(Int_t cent(0); cent < iNumCent; ++cent)
+  {
+    TH1D* raw_scaled = Scale((TH1D*) list_Raw_Cum_Charged->At(cent), hRaw_Mult->GetBinContent(cent+1)); if(!raw_scaled) { return; }
+    list_Raw_Cum_Charged_scaled->Add(raw_scaled);
 
-  TLegend* leg = new TLegend(0.1,0.5,0.6,0.89);
-  // leg->SetBorderWidth(0);
-  // leg->SetFillColorAplha(0,0);
-  leg->AddEntry(hRaw_Cum_Charged_cent0,"Unsub pPb","p");
-  leg->AddEntry(hBase_Cum_Charged_int,"pp (0-100%)","p");
-  leg->AddEntry(hBase_Cum_Charged_cent0,Form("pp (%s)",sCentLabel[0].Data()),"p");
-  leg->AddEntry(hBase_Cum_Charged_cent1,Form("pp (%s)",sCentLabel[1].Data()),"p");
-  leg->AddEntry(hBase_Cum_Charged_cent2,Form("pp (%s)",sCentLabel[2].Data()),"p");
-  leg->AddEntry(hBase_Cum_Charged_cent3,Form("pp (%s)",sCentLabel[3].Data()),"p");
+    TH1D* base_scaled = Scale((TH1D*) list_Base_Cum_Charged->At(cent), hBase_Mult->GetBinContent(cent+1)); if(!base_scaled) { return; }
+    list_Base_Cum_Charged_scaled->Add(base_scaled);
+  }
 
-  TCanvas* can = new TCanvas();
-  can->Divide(3,1);
-  can->cd(1);
-  TH1* frame = gPad->DrawFrame(0,0,10,0.2);
-  frame->SetTitle("raw <<2'>>");
-  hRaw_Cum_Charged_cent0->Draw("same");
-  hBase_Cum_Charged_int->Draw("same");
-  hBase_Cum_Charged_cent0->Draw("same");
-  hBase_Cum_Charged_cent1->Draw("same");
-  hBase_Cum_Charged_cent2->Draw("same");
-  hBase_Cum_Charged_cent3->Draw("same");
-  leg->Draw();
-  can->cd(2);
-  TH1* frame2 = gPad->DrawFrame(0,-0.03,10,1.0);
-  frame2->SetTitle("<M> * <<2'>>");
-  hRaw_Cum_Charged_cent0_scaled->Draw("same");
-  hBase_Cum_Charged_int_scaled->Draw("same");
-  hBase_Cum_Charged_cent0_scaled->Draw("same");
-  hBase_Cum_Charged_cent1_scaled->Draw("same");
-  hBase_Cum_Charged_cent2_scaled->Draw("same");
-  hBase_Cum_Charged_cent3_scaled->Draw("same");
-  can->cd(3);
-  TH1* frame3 = gPad->DrawFrame(0,-0.03,10,1.0);
-  frame3->SetTitle("<M>^{pPb}<<2'>>^{pPb} - <M>^{pp}<<2'>>^{pp} ");
-  hSubPP_Cum_Charged_int->Draw("same");
-  hSubPP_Cum_Charged_cent0->Draw("same");
-  hSubPP_Cum_Charged_cent1->Draw("same");
-  hSubPP_Cum_Charged_cent2->Draw("same");
-  hSubPP_Cum_Charged_cent3->Draw("same");
+  // subtracting (scaled) histos
+  TList* list_SubtPP_Cum_Charged = new TList();
 
-  can->SaveAs(Form("%s/Subt_pp-pbp_cent%d.pdf",sOutFolder.Data(),iCent),"pdf");
+  for(Int_t centRaw(0); centRaw < iNumCent; ++centRaw)
+  {
+    TH1D* temp_raw_cent = (TH1D*) list_Raw_Cum_Charged_scaled->At(centRaw);
+    TH1D* hSubPP_Cum_Charged_int = Subtract(temp_raw_cent, hBase_Cum_Charged_int_scaled);
+
+    TLegend* leg = new TLegend(0.1,0.5,0.6,0.89);
+    leg->AddEntry(temp_raw_cent,Form("Unsub pPb (%s)",sCentLabel[centRaw].Data()),"p");
+    leg->AddEntry(hBase_Cum_Charged_int,"pp (0-100%)","p");
+
+    TCanvas* can = new TCanvas("can","can",1200,400);
+    can->Divide(3,1);
+    can->cd(1);
+    TH1* frame = gPad->DrawFrame(0,0,10,0.2);
+    frame->SetTitle("raw <<2'>>");
+    ((TH1D*) list_Raw_Cum_Charged->At(centRaw))->Draw("same");
+    hBase_Cum_Charged_int->Draw("same");
+
+    can->cd(2);
+    TH1* frame2 = gPad->DrawFrame(0,-0.03,10,1.0);
+    frame2->SetTitle("<M> * <<2'>>");
+    temp_raw_cent->Draw("same");
+    hBase_Cum_Charged_int_scaled->Draw("same");
+
+    can->cd(3);
+    TH1* frame3 = gPad->DrawFrame(0,-0.3,10,0.5);
+    frame3->SetTitle("<M>^{pPb}<<2'>>^{pPb} - <M>^{pp}<<2'>>^{pp} ");
+    hSubPP_Cum_Charged_int->Draw("same");
+
+    for(Int_t cent(0); cent < iNumCent; ++cent)
+    {
+      TH1D* hSubPP_Cum_Charged = Subtract(temp_raw_cent, (TH1D*) list_Base_Cum_Charged_scaled->At(cent));
+      list_SubtPP_Cum_Charged->Add(hSubPP_Cum_Charged);
+
+      leg->AddEntry(hSubPP_Cum_Charged,Form("pp (%s)",sCentLabel[cent].Data()),"p");
+
+      can->cd(1);
+      ((TH1D*) list_Base_Cum_Charged->At(cent))->Draw("same");
+
+      can->cd(2);
+      ((TH1D*) list_Base_Cum_Charged_scaled->At(cent))->Draw("same");
+
+      can->cd(3);
+      hSubPP_Cum_Charged->Draw("same");
+    }
+
+    can->cd(1);
+    leg->Draw();
+    can->SaveAs(Form("%s/Subt_pp-pbp_cent%d.pdf",sOutFolder.Data(),centRaw),"pdf");
+
+  }
 
   return;
 }
