6d7bf666eeeb44cf0fc6bda4a75ef579d8374330
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-02-27 13:35:42 +0100

    CompreMultFluctuationTest plotting modified; 'NICE' ratio plots added

--- uniFlow/macros/flow-sub/refact/CompareMultFluctuationTest.C
+++ uniFlow/macros/flow-sub/refact/CompareMultFluctuationTest.C
@@ -17,6 +17,7 @@
 #include "TROOT.h"
 #include "TMath.h"
 #include "TColor.h"
+#include "TLine.h"
 
 TFile* OpenFile(TString sFileName, TString sMode = "READ");
 TH1D* LoadHisto(TString sHistName, TFile* file);
@@ -25,7 +26,8 @@ TH1D* DivideHistos(TH1D* nom, TH1D* denom, Bool_t bCor = kFALSE);
 void SetCustomPalette();
 
 // Color_t colors[] = {kGreen+2, kSpring+2, kCyan+2, kBlue+2, kMagenta+2, kRed-2, kRed+2, kOrange+2, kYellow+2, kOrange-4};
-Color_t colors[] = {kGreen+2, kGreen, kSpring+2, kSpring, kCyan+2, kCyan, kBlue+2, kBlue, kMagenta+2, kRed-2, kRed+2, kOrange+2, kYellow+2, kOrange-4,};
+// Color_t colors[] = {kGreen+2, kGreen, kSpring+2, kSpring, kCyan+2, kCyan, kBlue+2, kBlue, kMagenta+2, kRed-2, kRed+2, kOrange+2, kYellow+2, kOrange-4,};
+Color_t colors[] = {kRed, kBlue+2, kGreen, kOrange-3};
 
 
 void CompareMultFluctuationTest()
@@ -37,23 +39,23 @@ void CompareMultFluctuationTest()
   TString sGap = "gap00";
 
   Int_t iMultMin = 0;
-  Int_t iMultMax = 30;
-  Int_t iMultBinWidth = 5;
+  Int_t iMultBinWidth = 75;
+  Int_t iMultMax = 150/iMultBinWidth;
 
   // file with correlations in 0-20% (V0A) vs. N_RFP
-  TString sInputPathUnit = "/Users/vpacik/NBI/Flow/uniFlow/results/flowsub/pPb-NchRFP-gap00/output_vn_5/";
+  TString sInputPathUnit = Form("/Users/vpacik/NBI/Flow/uniFlow/results/flowsub/pPb-NchRFP-gap00/output_vn_%d/",iMultBinWidth);
   // file with correlations in 0-20% (V0A)  (i.e vs centrality percentile)
   TString sInputPathMerged = "/Users/vpacik/NBI/Flow/uniFlow/results/flowsub/pPb-NchRFP-gap00/output_vn_merged/";
 
-  TString sOutputPath = "/Users/vpacik/NBI/Flow/uniFlow/results/flowsub/CompareMultFluctuationTest/pPb_gap00/5/";
+  TString sOutputPath = Form("/Users/vpacik/NBI/Flow/uniFlow/results/flowsub/CompareMultFluctuationTest/pPb_gap00/%d/",iMultBinWidth);
   // ##########################################################################################################
   gSystem->mkdir(sOutputPath.Data(),kTRUE);
 
   // gStyle->SetPalette(1);
   SetCustomPalette();
 
-  Double_t dYmin = -100.0;
-  Double_t dYmax = 100.0;
+  Double_t dYmin = -10.0;
+  Double_t dYmax = 20.0;
   Double_t dXmin = 0.0;
   Double_t dXmax = 10.0;
 
@@ -68,43 +70,67 @@ void CompareMultFluctuationTest()
   canAll->cd(4);
   TH1* frame_canAll_4 = (TH1*) gPad->DrawFrame(dXmin,dYmin,dXmax,dYmax);
 
-  TCanvas* canAllRatio = new TCanvas("canAllRatio","canAllRatio",800,800);
-  canAllRatio->cd(1);
-  TH1* frame_canAllRatio_1 = (TH1*) gPad->DrawFrame(dXmin,0.0,dXmax,2.0);
+  TLine* lineUnity = new TLine(dXmin,1.0,dXmax,1.0);
+  lineUnity->SetLineColor(kGray+2);
+  lineUnity->SetLineStyle(kDashed);
 
-    // Int_t iMult = 0;
+  TLegend* legRatio = new TLegend(0.12,0.75,0.4,0.89);
+  legRatio->SetBorderSize(0);
+  legRatio->SetFillColorAlpha(0.,0.);
+  legRatio->SetTextFont(42);
+  legRatio->SetTextSize(0.03);
+  legRatio->SetHeader(Form("Bin width %d",iMultBinWidth));
+
+  TCanvas* canRatio = new TCanvas("canRatio","canRatio",800,800);
+  TH1* frame_canRatio = (TH1*) gPad->DrawFrame(dXmin,0.8,dXmax,1.2);
+  legRatio->Draw();
+  lineUnity->Draw("same");
+
+
+
   for(Int_t iMethod(0); iMethod < iNumMethods; ++iMethod)
   {
-    TLegend* leg = new TLegend(0.0,0.0,0.32,1.0);
+    TLegend* leg = new TLegend(0.12,0.75,0.4,0.89);
     leg->SetBorderSize(0);
     leg->SetFillColorAlpha(0.,0.);
-    leg->SetHeader("N_{RFP} bin");
+    leg->SetHeader(sMethods[iMethod].Data());
     leg->SetTextFont(42);
     leg->SetTextSize(0.03);
 
-    TLegend* leg2 = new TLegend(0.33,0.0,0.65,1.0);
-    leg2->SetBorderSize(0);
-    leg2->SetFillColorAlpha(0.,0.);
-    leg2->SetHeader("N_{RFP} bin");
-    leg2->SetTextFont(42);
-    leg2->SetTextSize(0.03);
+    TCanvas* canDiff = new TCanvas(Form("canDiff"),Form("canDiff"),500,1200);
+    canDiff->cd();
+    TPad* padMain = new TPad("padMain","padMain", 0, 0.3, 1, 1.0);
+    padMain->SetBottomMargin(0.0);
+    padMain->SetRightMargin(0.03);
+    padMain->Draw();
+    padMain->cd();
+    TH1* frame_canDiff_1 = (TH1*) gPad->DrawFrame(dXmin,-1.05,dXmax,1);
+    frame_canDiff_1->SetTitle(Form("%s v_{2}{2,%s} (0-20%% V0A) ; ; v_{2}{2}",sSpecies.Data(),sGap.Data()));
+    frame_canDiff_1->SetTitleFont(43,"X");
+    frame_canDiff_1->SetTitleSize(18,"X");
+    frame_canDiff_1->SetTitleOffset(4.3,"X");
+    frame_canDiff_1->SetLabelFont(43,"X");
+    frame_canDiff_1->SetLabelSize(18,"X");
+    leg->Draw();
 
-    TLegend* leg3 = new TLegend(0.66,0.0,1.0,1.0);
-    leg3->SetBorderSize(0);
-    leg3->SetFillColorAlpha(0.,0.);
-    leg3->SetHeader("N_{RFP} bin");
-    leg3->SetTextFont(42);
-    leg3->SetTextSize(0.03);
+    canDiff->cd();
+    TPad* padRatio = new TPad("padRatio","padRatio", 0, 0.0, 1, 0.3);
+    padRatio->SetTopMargin(0.0);
+    padRatio->SetBottomMargin(0.25);
+    padRatio->SetRightMargin(0.03);
+    padRatio->Draw();
+    padRatio->cd();
+    TH1* frame_canDiff_2 = (TH1*) gPad->DrawFrame(dXmin,0.8,dXmax,1.25);
+    frame_canDiff_2->SetTitle(Form("; p_{T} (GeV/c); mean / integrated   "));
+    frame_canDiff_2->SetNdivisions(505,"Y");
+    frame_canDiff_2->SetTitleFont(43,"XY");
+    frame_canDiff_2->SetTitleSize(18,"XY");
+    frame_canDiff_2->SetTitleOffset(4.3,"X");
+    frame_canDiff_2->SetTitleOffset(1.5,"Y");
+    frame_canDiff_2->SetLabelFont(43,"XY");
+    frame_canDiff_2->SetLabelSize(18,"XY");
+    lineUnity->Draw("same");
 
-    TCanvas* canDiff = new TCanvas(Form("canDiff"),Form("canDiff"),2400,800);
-    canDiff->Divide(3,1);
-    canDiff->cd(1);
-    TH1* frame_canDiff_1 = (TH1*) gPad->DrawFrame(0.0,-1,10.0,1);
-    frame_canDiff_1->SetTitle(Form("%s v_{2}{2,%s} (0-20%% V0A) ; p_{T} (GeV/c); v_{2}{2}",sSpecies.Data(),sGap.Data()));
-
-    canDiff->cd(2);
-    TH1* frame_canDiff_2 = (TH1*) gPad->DrawFrame(0.0,0.5,10.0,1.5);
-    frame_canDiff_2->SetTitle(Form("; p_{T} (GeV/c); ratio"));
 
     TFile* fileUnit = OpenFile(sInputPathUnit+sMethods[iMethod]+"/Processed.root","READ"); if(!fileUnit) return;
     TFile* fileMerged = OpenFile(sInputPathMerged+sMethods[iMethod]+"/Processed.root","READ"); if(!fileMerged) return;
@@ -113,24 +139,19 @@ void CompareMultFluctuationTest()
     StyleHist(histMerged,kRed,kFullCircle);
     leg->AddEntry(histMerged,"integrated","p");
 
-    Int_t nPnt  = iMultMax-iMultMin;
-    Int_t nnCol = gStyle->GetNumberOfColors();
-
-
-    // const Int_t iBins = histMerged->GetXaxis()->GetNbins();
-    // const Double_t* bins = histMerged->GetXaxis()->GetXbins()->GetArray();
+    TProfile* p_weight_sigma = new TProfile("p_weight_sigma","p_weight_sigma",histMerged->GetXaxis()->GetNbins(),histMerged->GetXaxis()->GetXbins()->GetArray());
+    if(!p_weight_sigma) { printf("Profile does not exists!\n"); return; }
+    p_weight_sigma->Sumw2();
+    StyleHist(p_weight_sigma,kBlack,kOpenSquare);
 
     TProfile* p_weight_average = new TProfile("p_weight_average","p_weight_average",histMerged->GetXaxis()->GetNbins(),histMerged->GetXaxis()->GetXbins()->GetArray());
     if(!p_weight_average) { printf("Profile does not exists!\n"); return; }
     p_weight_average->Sumw2();
     StyleHist(p_weight_average,kMagenta+2,kOpenSquare);
-    leg->AddEntry(p_weight_average,"simple average","p");
 
-    TProfile* p_weight_sigma = new TProfile("p_weight_sigma","p_weight_sigma",histMerged->GetXaxis()->GetNbins(),histMerged->GetXaxis()->GetXbins()->GetArray());
-    if(!p_weight_sigma) { printf("Profile does not exists!\n"); return; }
-    p_weight_sigma->Sumw2();
-    StyleHist(p_weight_sigma,kBlack,kOpenSquare);
-    leg->AddEntry(p_weight_sigma,"1/#sigma^{2}","p");
+
+    Int_t nPnt  = iMultMax-iMultMin;
+    Int_t nnCol = gStyle->GetNumberOfColors();
 
     for(Int_t iMult(iMultMin); iMult < iMultMax; ++iMult)
     {
@@ -138,21 +159,14 @@ void CompareMultFluctuationTest()
 
       TH1D* histUnit = LoadHisto(sHistoName,fileUnit); if(!histUnit) return;
 
-
-      TLegend* curLeg = 0x0;
-      if(iMult < 0.33*nPnt) { curLeg = leg; }
-      else if(iMult < 0.66*nPnt) { curLeg = leg2; }
-      else { curLeg = leg3; }
-      curLeg->AddEntry(histUnit,Form("%d",iMult*iMultBinWidth),"p");
-
       Int_t idx = iMult * Float_t(nnCol-1) / (nPnt-1);
       StyleHist(histUnit, gStyle->GetColorPalette(idx), kOpenCircle);
 
-      canDiff->cd(1);
+      padMain->cd();
       histUnit->Draw("hist p x0 same");
+      if(iMult == iMultMin) { leg->AddEntry(histUnit,"N_{RFP} bins","p"); }
       canAll->cd(iMethod+1);
       histUnit->Draw("hist p e1 same");
-
 
       for(Int_t pt(0); pt < histUnit->GetNbinsX()+1; ++pt)
       {
@@ -160,59 +174,59 @@ void CompareMultFluctuationTest()
         Double_t dValue = histUnit->GetBinContent(pt);
         Double_t dError = histUnit->GetBinError(pt);
 
-        if(dError <= 0.0) continue;
-        if(dValue == 9.0 && dError == 9.0) continue;
+        if(dError <= 0.0001) continue;
+        if(dValue >= 8.0 && dError >= 8.0) continue;
 
         Double_t dWeight = TMath::Power(dError,-2.0);
         p_weight_average->Fill(bin,dValue,1.0);
         p_weight_sigma->Fill(bin,dValue,dWeight);
-        printf("Val %f | weight %f\n",dValue,dWeight);
+        if(iMethod == 0 && pt == histUnit->GetNbinsX() )
+        {
+            printf("pT %d: Val %f | weight %f\n",pt,dValue,dWeight);
+            printf("%f +- %f \n", dValue, dError);
+            if(dError <= 0.0000001) printf("ZERO \n");
+
+        }
       }
 
     }
+
+    leg->AddEntry(p_weight_sigma,"1/#sigma^{2} weighted mean","p");
+    leg->AddEntry(p_weight_average,"arithmeric mean","p");
+
+    // making histos out of profiles (for "safety" reason)
     TH1D* h_weight_average = (TH1D*) p_weight_average->ProjectionX();
     TH1D* h_weight_sigma = (TH1D*) p_weight_sigma->ProjectionX();
     StyleHist(h_weight_average,kMagenta+2,kOpenSquare);
     StyleHist(h_weight_sigma,kBlack,kOpenSquare);
 
-    canDiff->cd(1);
+    padMain->cd();
     histMerged->Draw("same");
     h_weight_average->Draw("same");
     h_weight_sigma->Draw("same");
 
+    canAll->cd(iMethod+1);
+    histMerged->Draw("same");
+    h_weight_average->Draw("same");
+    h_weight_sigma->Draw("same");
+    leg->Draw();
 
-
-    canDiff->cd(2);
+    padRatio->cd();
     TH1D* hRatioSigma = DivideHistos(h_weight_sigma,histMerged);
     TH1D* hRatioAverage = DivideHistos(h_weight_average,histMerged);
     hRatioSigma->Draw("hist p e1 same");
     hRatioAverage->Draw("hist p e1 same");
 
-    canDiff->cd(3);
-    leg->Draw();
-    leg2->Draw();
-    leg3->Draw();
-
     canDiff->SaveAs(Form("%s/%s_%s.pdf",sOutputPath.Data(),sSpecies.Data(),sMethods[iMethod].Data()),"pdf");
 
-    // TCanvas* canProf = new TCanvas();
-    // canProf->cd();
-    // prof->Draw();
-    canAll->cd(iMethod+1);
-    histMerged->Draw("same");
-    p_weight_average->Draw("same");
-    p_weight_sigma->Draw("same");
-
-    // canAll->cd(4);
-    // leg->Draw();
-
-    canAllRatio->cd();
-    hRatioSigma->Draw("same");
-    hRatioAverage->Draw("same");
+    canRatio->cd();
+    StyleHist(hRatioSigma,colors[iMethod],kOpenSquare);
+    hRatioSigma->Draw("hist p e1 same");
+    legRatio->AddEntry(hRatioSigma,sMethods[iMethod].Data(),"p");
   }
-  canAll->SaveAs(Form("%s/%s_%s.pdf",sOutputPath.Data(),sSpecies.Data(),"compall"),"pdf");
-  canAllRatio->SaveAs(Form("%s/%s_%s.pdf",sOutputPath.Data(),sSpecies.Data(),"compallratio"),"pdf");
 
+  canAll->SaveAs(Form("%s/%s_%s.pdf",sOutputPath.Data(),sSpecies.Data(),"ALL"),"pdf");
+  canRatio->SaveAs(Form("%s/%s_%s.pdf",sOutputPath.Data(),sSpecies.Data(),"RATIO"),"pdf");
   return;
 }
 // ==================================================================================================================
